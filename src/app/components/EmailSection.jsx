04f1add0c7a0748bb2679071540ae39eabd28279
hopeifeanyi1 <hopeifeanyi2@gmail.com>
2024-08-08 22:21:54 +0100

    added link to github and linkedin

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -54,10 +54,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
             <div className='socials flex flex-row gap-2'>
-              <Link href="github.com">
+              <Link href="https://github.com/hopeifeanyi1">
                 <Image src={GithubIcon} alt="Github Icon" />
               </Link>
-              <Link href="linkedin.com">
+              <Link href="https://www.linkedin.com/in/hope-ifeanyi">
                 <Image src={LinkedinIcon} alt="Linkedin Icon" />
               </Link>
             </div>
