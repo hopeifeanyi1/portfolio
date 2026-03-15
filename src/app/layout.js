e997bb4c4701559969aa5f5e2e650c85bc9dc847
hopeifeanyi1 <hopeifeanyi2@gmail.com>
2026-03-15 12:00:01 +0100

    analytics and next version upgrade

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,32 @@
 //src/app/layout.js
-import './globals.css'
-import { Inter } from 'next/font/google'
-import { Toaster } from '@/components/ui/sonner'
-import NavBar from './components/NavBar'
-import CursorSpotlight from './components/CursorSpotlight'
-import ChatBot from './components/ChatBot'
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/sonner";
+import NavBar from "./components/NavBar";
+import CursorSpotlight from "./components/CursorSpotlight";
+import ChatBot from "./components/ChatBot";
+import { Analytics } from "@vercel/analytics/next";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Hope's Portfolio",
   description: "Ifeanyi Hope Portfolio website",
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang='en'>
-      <body className={`flex min-h-screen flex-col bg-white text-black dark:bg-black dark:text-white ${inter.className}`}>
+    <html lang="en">
+      <body
+        className={`flex min-h-screen flex-col bg-white text-black dark:bg-black dark:text-white ${inter.className}`}
+      >
         <CursorSpotlight />
         <NavBar />
         {children}
         <ChatBot />
         <Toaster position="bottom-left" />
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
